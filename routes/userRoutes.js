f07d4545cc02f4436b7156600ac67e5c79f0ce69
rajabrajabb <a.ragab23@fatoorah.sa>
2024-07-29 23:56:14 +0300

    sad

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const authController = require('../controllers/authControllers')
+const express = require("express");
+const authController = require("../controllers/authControllers");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup)
-router.post('/login', authController.login)
-router.post('/logout', authController.logout)
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.post("/logout", authController.logout);
 
-router.get('/userProfile', authController.isLoggedIn, authController.userProfile)
+router.get(
+  "/userProfile",
+  authController.isLoggedIn,
+  authController.userProfile
+);
 
-router.post('/updateUser/:id', authController.update)
-router.delete('/deleteUser/:id', authController.delete)
-
+router.patch("/updateUser/:id", authController.update);
+router.delete("/deleteUser/:id", authController.delete);
 
 module.exports = router;
-
